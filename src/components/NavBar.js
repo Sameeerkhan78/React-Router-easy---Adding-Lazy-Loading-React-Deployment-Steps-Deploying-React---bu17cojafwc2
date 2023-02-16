f2aa5f8dd07e80ec1f170f363963ad5972deb5ff
Sameer Khan <115018276+Sameeerkhan78@users.noreply.github.com>
2023-02-16 20:52:15 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,5 @@
-import React from 'react';
-
-
-export const NavBar = () =>{
-    return(
-
-    )
-}
+import React from "react";
+const NotFound = () => {
+  return <div id="not-found-page">Not found page</div>;
+};
+export default NotFound;
